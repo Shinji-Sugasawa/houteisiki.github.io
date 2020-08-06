ab05e42c7e7810096d0f97350663be7e23c41bb7
Shinji-Sugasawa <68045030+Shinji-Sugasawa@users.noreply.github.com>
2020-08-06 21:01:24 +0900

    Create Newton.java

---
+++ Newton.java
@@ -0,0 +1,39 @@
+package houteisiki;
+
+public class Newton {
+
+	double func_y(double x) {
+
+		return x*x*x + x -1.0;//求めたい方程式
+	}
+	double func_z(double x) {
+
+		return 3.0*x*x + 1.0;
+	}
+	double syori(double a, double eps) {
+		int i = 1;
+		double b = .0;
+
+		while(i>=1) {
+			b = a-func_y(a) / func_z(a);
+
+        	System.out.println( i+ ":" + b );//計算出力
+
+			if( Math.abs( b - a ) < eps) {
+
+	        	System.out.println("\n近似解" + b );//結果表示
+				break;
+			}
+			else{
+				a = b;
+			}
+			i++;
+		}
+		return b;
+	}
+	public static void main (String[] args){
+        Newton kai = new Newton();
+
+        double ans = kai.syori(1.0, 0.0001);// 解を計算(初期値, 収束条件)
+    }
+}
