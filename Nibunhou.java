b163cef598aade517fc99a187a205e70679d409a
Shinji-Sugasawa <68045030+Shinji-Sugasawa@users.noreply.github.com>
2020-08-06 17:26:09 +0900

    Update Nibunhou.java

--- Nibunhou.java
+++ Nibunhou.java
@@ -5,6 +5,13 @@ public class Nibunhou {
 	class func_y(double x) {
 		return x*x*x + x - 1.0;
 	}
-	class
+	class do (double a ,double b, double eps) {
+		double c = ( a + b ) /2.0;
+		if(( func_y(c) * func_y(a) ) < 0 ) {
+			b = c;
+		}else {
+			a = c;
+		}
+	}
 
 }
