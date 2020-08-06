4f628b07c6d4308d3e073810bd1eb047c0fa9dbf
Shinji-Sugasawa <68045030+Shinji-Sugasawa@users.noreply.github.com>
2020-08-06 17:39:44 +0900

    Update Nibunhou.java

--- Nibunhou.java
+++ Nibunhou.java
@@ -5,7 +5,10 @@ public class Nibunhou {
 	class func_y(double x) {
 		return x*x*x + x - 1.0;
 	}
-	class do (double a ,double b, double eps) {
+	class keisan(double a ,double b, double eps) {
+
+		int i = 0;
+
 		while(Math.abs(a-b) < eps) {
 
 			double c = ( a + b ) /2.0;
@@ -16,8 +19,18 @@ public class Nibunhou {
 			else {
 				a = c;
 			}
-
+			System.out.println( i+ ":" +c );//計算回数出力
 		}
+		return i;
+	}
+
+	public static void main(String args[]) {
+
+		Nibunhou kai = new Nibunhou();
+
+		int ans = kai.keisan(0.0, 4.0, 0.001)ans;//(原点,初期値,範囲誤差)
+
+		System.out.println("計算回数:" +ans);//計算回数出力
 	}
 
 }
