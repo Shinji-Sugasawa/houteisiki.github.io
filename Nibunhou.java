57593b83645fac0a9bad1a259f47c60082dcca1b
Shinji-Sugasawa <68045030+Shinji-Sugasawa@users.noreply.github.com>
2020-08-06 23:55:50 +0900

    Update Nibunhou.java

--- Nibunhou.java
+++ Nibunhou.java
@@ -3,8 +3,8 @@ package houteisiki;
 public class Nibunhou {
 
 	double func_y(double x){
-        return x*x*x + x -1.0;//求めたい方程式
-    }
+        return Math.pow(x, 3)+ x -1.0;//求めたい方程式
+    }//Math.pow(x.指数)　＜＝ xの指数乗を表している。
 
     double syori(double a, double b, double eps){
         int i=1;
@@ -30,7 +30,7 @@ public class Nibunhou {
         public static void main (String[] args) {
             Nibunhou kai = new Nibunhou();
 
-            double eps = 0.0001;//許容誤差
+            double eps = 0.0001;//許容誤差 //例0.000001;
             double a = 0.0, b = 1.0;
 
             double ans = kai.syori(a, b, eps);//(初期値,許容誤差)
